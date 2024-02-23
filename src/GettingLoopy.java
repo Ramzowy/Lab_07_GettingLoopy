695273963a2e1515ae1035574ac5eace645935f9
Ramzowy <ramzowy@gmail.com>
2024-02-23 13:03:29 -0500

    Lab_07_loopy

--- src/GettingLoopy.java
+++ src/GettingLoopy.java
@@ -3,14 +3,65 @@ public class GettingLoopy {
 
       int i;
       int m;
-        for (i=30; i >= 0 && i<=30; i--) {
-//            for ( m=1; m<= i; m++) {
+        for (i=0; i >= 0 && i<=30; i++) {
                 System.out.print(i);
-//            }
+
             System.out.println();
-        }
 
 
 
     }
+
+        for (i=30; i >= 0 && i<=30; i--) {
+                System.out.print(i);
+
+               System.out.println();
+           }
+
+            for (i=0; i >= 0 && i<=18; i+=3) {
+                for (m = 1; m <= i; m++) {
+                    System.out.print(i);
+
+                    System.out.println();
+                }
+
+
+            }
+        for (i=10; i >= 0 && i<=10; i-=2) {
+                    System.out.print(i);
+
+                    System.out.println();
+                }
+
+            for (i=0; i >= 0 && i<=18; i+=3) {
+                for (m = 1; m <= i; m++) {
+                    System.out.print(i);
+
+                    System.out.println();
+                }
+            }
+
+        for (i=0; i >= 0 && i<=5; i++) {
+                for ( m=1; m<= i; m++) {
+                    System.out.print("*");
+                }
+                    System.out.println();
+                }
+
+        for (i=5; i >= 0 && i<=5; i--) {
+            for ( m=1; m <= i; m++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (i=1; i >= 0 && i<=5; i++) {
+            for ( m=1; m <= 5; m++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+     }
 }
