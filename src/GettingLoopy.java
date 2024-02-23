ee3b89f40e57247e6ae80aa00bf9e6328258af90
Ramzowy <ramzowy@gmail.com>
2024-02-23 12:40:49 -0500

    Lab_07

---
+++ src/GettingLoopy.java
@@ -0,0 +1,16 @@
+public class GettingLoopy {
+    public static void main(String[] args) {
+
+      int i;
+      int m;
+        for (i=30; i >= 0 && i<=30; i--) {
+//            for ( m=1; m<= i; m++) {
+                System.out.print(i);
+//            }
+            System.out.println();
+        }
+
+
+
+    }
+}
